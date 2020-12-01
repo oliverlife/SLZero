8123772163763ee86370aa5b438397c5fa380672
oliverlife <oliverlife@foxmail.com>
2020-12-01 08:34:11 +0800

    feature: 添加测试数据

--- SLZeroTests/SLZeroTests.m
+++ SLZeroTests/SLZeroTests.m
@@ -83,10 +83,19 @@
     XCTAssertEqualObjects(game, game2);
 }
 
+- (NSString *)testFilePath: (NSString *)name {
+    return [NSString stringWithFormat:@"%@/%@", [NSBundle bundleForClass:SLZeroTests.class].bundlePath, name];
+}
+
 - (void)testPerformanceExample {
+    NSLog(@"%@", [NSBundle bundleForClass:SLZeroTests.class]);
+    NSLog(@"%@", [NSBundle mainBundle]);
+    NSString * path = [self testFilePath:@"game.w20.h10.m40.plist"];
+    SLOGame * game = [SLOGame fromWithData: [NSFileManager.defaultManager contentsAtPath:path]];
     // This is an example of a performance test case.
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
+        NSUInteger a = 1 + 1;
     }];
 }
 
